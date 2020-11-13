092bb139b9b1d04a2fce4bd463b72101d0cb99f0
Герций Елена <gertsij.elena@gmail.com>
2020-11-13 11:53:40 +0200

    write html page on desktop

--- Crypto/Program.cs
+++ Crypto/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+//using System.Globalization;
+//using System.Linq;
+using System.Net;
+//using System.Threading.Tasks;
 
 namespace Crypto
 {
@@ -10,6 +15,47 @@ namespace Crypto
     {
         static void Main(string[] args)
         {
+            string url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DASH&tsyms=USD";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/json; charset=utf-8";
+            request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("username:password"));
+            request.PreAuthenticate = true;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                string list = reader.ReadToEnd();
+                //Console.WriteLine(list);
+
+                Dictionary<string, object> objectlist = JsonConvert.DeserializeObject<Dictionary<string, object>>(list);
+
+                string[] tickers = new string[] { "BTC", "ETH", "DASH" };
+
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                using (FileStream fs = new FileStream(path + "/answer.htm", FileMode.OpenOrCreate))
+                {
+
+                    using (StreamWriter w = new StreamWriter(fs, Encoding.UTF8))
+                    {
+                        w.WriteLine("<H1>List for Cryptoprices</H1>");
+
+                        foreach (string t in tickers)
+                        {
+                            string pr = objectlist[t].ToString();
+                            pr = pr.Trim(new char[] { '{', '}' });
+                            int s = pr.IndexOf(':');
+                            pr = pr.Remove(1, s);
+
+                            w.WriteLine("<table><tr><td>" + t + "</td ><td>" + pr + "</td></tr></table>");
+
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine("Тhe information has saved in file answer.htm on Desktop.Plese press any key to complete.");
+            Console.ReadKey();
         }
     }
 }
